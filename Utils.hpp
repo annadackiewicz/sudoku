81c9ed49460f24a082c343d0cdbc4abcadf90e6b
Anna Dackiewicz <anna.dackiewicz@gmail.com>
2013-07-10 00:05:58 +0200

    Forgotten file of Utils

---
+++ Utils.hpp
@@ -0,0 +1,12 @@
+#ifndef _SUDOKU_UTILS_HPP_
+#define _SUDOKU_UTILS_HPP_
+
+const int board_x_size = 9;
+const int board_y_size = 9;
+const int square_x_size = 3;
+const int square_y_size = 3;
+
+const int max_num = 9;
+const int min_num = 0;
+
+#endif  // _SUDOKU_UTILS_HPP
